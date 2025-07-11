1ae4d49422e13f8ca1076c7a3541cf74bc4c19fd
Chamdarang-! <joseph8034@naver.com>
2025-07-11 17:49:06 +0900

    add codingtest

--- CodingTest/src/step1/Main07.java
+++ CodingTest/src/step1/Main07.java
@@ -1,5 +1,14 @@
 package step1;
+import java.util.Scanner;
 
 public class Main07 {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		
+		//서기 연도
+		int A = y-543;
+		System.out.println(A);
+	}
 }
--- CodingTest/src/step1/Main10.java
+++ CodingTest/src/step1/Main10.java
@@ -1,5 +1,32 @@
 package step1;
 
+import java.util.Scanner;
+
 public class Main10 {
+	
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");
+		int a = sc.nextInt();
+		
+		System.out.print("두번째 숫자 첫번째 자리를 입력하세요 : ");
+		int b = sc.nextInt();
+		
+		System.out.print("두번째 숫자 두번째 자리를 입력하세요 : ");
+		int c = sc.nextInt();
+		
+		System.out.print("두번째 숫자 세번째 자리를 입력하세요 : ");
+		int d = sc.nextInt();
+		
+		int e = ((a*b)+(a*c)+(a*d));
+		
+		System.out.println(a*b);
+		System.out.println(a*c);
+		System.out.println(a*d);
+		System.out.println(e);
+		
+	}
 
 }
