364ca00418687be1094eca4010c669cbce4693ee
Chamdarang-1 <joseph8034@naver.com>
2025-07-23 23:56:36 +0900

    update

---
+++ Rensyuu/src/Arrayrensyuu/Array.java
@@ -0,0 +1,22 @@
+package Arrayrensyuu;
+
+public class Array {
+	
+	public static void main(String[] args) {
+		
+		int[] arr1 = {1, 2, 3, 4, 5};
+		char[] arr2 = {'A','B','C'};
+		String[] arr3 = {"서울","대전","대구","부산","광주"};
+		boolean[] arr4 = {true, false, true, true, false};
+		
+		// arr1 = 정수형 배열
+		// arr2 = 문자형 배열
+		// arr3 = 문자열 배열
+		// aar4 = 논리형 배열
+	
+		
+		System.out.println("arr1[0] : " + arr1[0]);
+		
+		
+	}
+}
